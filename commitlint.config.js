e0a2dd9f0ef58ebb766257aa3b99d0e1b4e965f9
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2024-10-08 00:39:11 +0100

    build: install commitlint,husky,semantic-release

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+  extends: ["@commitlint/config-angular"],
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "body-max-line-length": [2, "always", 72],
+    "header-full-stop": [2, "never", "."],
+    "body-leading-blank": [2, "always"],
+  },
+};
