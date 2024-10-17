4e537db2a62ee092830d271e77eb76bad6d50a7e
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2024-10-17 20:34:03 +0100

    fix: changes in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,13 @@
 /* eslint-env node */
 module.exports = {
   extends: ["@commitlint/config-angular"],
+  parserPreset: "conventional-changelog-angular",
+  ignores: [
+    (commitmsg) => {
+      const regex = /^Merge branch '(?:.*)' into (?:.*)$/;
+      regex.test(commitmsg);
+    },
+  ],
   rules: {
     "header-max-length": [2, "always", 72],
     "body-max-line-length": [2, "always", 72],
